b11961ecb196d6f0cc3ace00d9ada18adae1e10f
duong130125 <nguyendinhduong2005yt@gmail.com>
2024-03-29 10:50:47 +0700

    add

--- js/signup.js
+++ js/signup.js
@@ -1,24 +1,24 @@
-const signUpButton = document.getElementById('signUpButton');
-const signupForm = document.getElementById('signupForm');
-const successMessage = document.getElementById('successMessage');
-const errorMessage = document.getElementById('errorMessage');
-const infoMessage = document.getElementById('infoMessage');
-const loginLink = document.getElementById('loginLink');
+let signUpButton = document.getElementById('signUpButton');
+let signupForm = document.getElementById('signupForm');
+let successMessage = document.getElementById('successMessage');
+let errorMessage = document.getElementById('errorMessage');
+let infoMessage = document.getElementById('infoMessage');
+let loginLink = document.getElementById('loginLink');
 
 signUpButton.addEventListener('click', function(event) {
     event.preventDefault(); 
 
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
 
     if (!firstName || !lastName || !email || !password) {
         alert("Vui lòng điền đầy đủ thông tin");
         return; 
     }
 
-    const formData = {
+    let formData = {
         id: Math.floor(Math.random()*999999),
         firstName: firstName,
         lastName: lastName,
@@ -29,7 +29,7 @@ signUpButton.addEventListener('click', function(event) {
 
     let formDataArray = JSON.parse(localStorage.getItem('formDataArray')) || [];
 
-    const emailExists = formDataArray.some(data => data.email === email);
+    let emailExists = formDataArray.some(data => data.email === email);
 
     if (emailExists) {
         alert("Email đã tồn tại");
